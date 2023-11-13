0b2df299074eb9267b45e075067725961c3c716c
youdonghee <0103donghee@naver.com>
2023-11-13 10:31:00 +0900

    wallet conncect ing

--- src/wagmi-config.ts
+++ src/wagmi-config.ts
@@ -1,10 +1,32 @@
 import { configureChains, createConfig } from "wagmi";
 import { publicProvider } from "wagmi/providers/public";
-import { polygon, mainnet } from "wagmi/chains";
+import { polygon, mainnet, Chain } from "wagmi/chains";
 import { MetaMaskConnector } from "wagmi/connectors/metaMask";
 
+const customChain: Chain = {
+  id: 18328, // 여기에 회사에서 제공한 체인 ID를 입력
+  name: "BounsWalletChain",
+  network: "BounsNetwork",
+  nativeCurrency: {
+    name: "BounsNetworkCoin", // 해당 체인의 네이티브 통화명
+    symbol: "BNC", // 해당 체인의 네이티브 통화 심볼
+    decimals: 18, // 해당 체인의 네이티브 통화 소수점 자릿수
+  },
+  rpcUrls: {
+    default: { http: ["https://network.bouncecode.net/"] }, // 여기에 회사에서 제공한 RPC URL을 입력
+    public: { http: ["https://network.bouncecode.net/"] }, // 공개 RPC URL을 배열로 제공
+  },
+  blockExplorers: {
+    default: {
+      name: "BounsWallet Explorer",
+      url: "https://network.bouncecode.net/",
+    },
+  },
+  testnet: true, // 해당 체인이 테스트넷인 경우 true로 설정
+};
+
 const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [mainnet, polygon],
+  [mainnet, customChain],
   [publicProvider()]
 );
 
