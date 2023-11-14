2004b5b7857be2a4f8c61c60efc7909cd1a9fc82
wijiwon <dnlwldnjs5@gmail.com>
2023-11-14 20:28:36 +0900

    main pull

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <div className="App ">
-<<<<<<< HEAD
+        {/* <<<<<<< HEAD */}
         {!isSNSLoggedIn ? (
           <SNSLogin onLoginSuccess={() => setIsSNSLoggedIn(true)} />
         ) : (
@@ -56,21 +56,21 @@ function App() {
             <Footer />
           </>
         )}
-=======
-        <WagmiConfig config={config}>
-          <div className={Divstyle.header_body}>
-            <ToggleBtn />
-            <HeaderBox />
-            <Routes>
-              <Route path="/poolpair" element={<Poolpair />} />
-              <Route path="/tokendetail" element={<TokenDetail />} />
-              <Route path="/stake" element={<Stake />} />
-              <Route path="/stake/:id" element={<StakeDetail />} />
-            </Routes>
-          </div>
-          <Footer />
-        </WagmiConfig>
->>>>>>> origin/main
+        {/* // =======
+//         <WagmiConfig config={config}>
+//           <div className={Divstyle.header_body}>
+//             <ToggleBtn />
+//             <HeaderBox />
+//             <Routes>
+//               <Route path="/poolpair" element={<Poolpair />} />
+//               <Route path="/tokendetail" element={<TokenDetail />} />
+//               <Route path="/stake" element={<Stake />} />
+//               <Route path="/stake/:id" element={<StakeDetail />} />
+//             </Routes>
+//           </div>
+//           <Footer />
+//         </WagmiConfig>
+// >>>>>>> origin/main */}
       </div>
     </QueryClientProvider>
   );
