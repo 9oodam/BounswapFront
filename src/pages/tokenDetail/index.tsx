28685d3262bca77ca13c0491ce19cfa65831dbf5
ahyeona <ahhyeon741@gmail.com>
2023-11-23 21:34:50 +0900

    data Features 작성. getAllTokens, getEachToken, getEachPool 작성

--- src/pages/tokenDetail/index.tsx
+++ src/pages/tokenDetail/index.tsx
@@ -20,6 +20,7 @@ const TokenDetail: React.FC = () => {
   const queryClient = useQueryClient();
   const { id } = useParams();
 
+  // 컨트랙트 호출 테스트
   useEffect(()=>{
     if (!dataContract || !id || !web3) return;
     const getData =async () => {
