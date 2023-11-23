78f07467fdc1cc504ba944044b46e4289c379f0d
youdonghee <0103donghee@naver.com>
2023-11-23 15:59:10 +0900

    Swap 1차 완료

--- src/pages/Swap/index.tsx
+++ src/pages/Swap/index.tsx
@@ -1,9 +1,10 @@
 import SwapContainer from "../../components/SwapContainer";
 import React, { useEffect, useState } from "react";
 import Card from "src/components/Card";
+import CustomModal from "./CustomModal";
 import TokenInput from "src/contents/Swap/TokenInput";
+import SwapBtn from "src/contents/poolpair/Liquidity/LiquidiityBtn/SwapBtn";
 import SwapFetchingCard from "src/components/Card/SwapFetchingCard";
-import SwapButton from "src/contents/Swap/SwapButton";
 
 type Token = {
   tokenAddress: string;
@@ -90,11 +91,24 @@ const Swap = () => {
             selectedToken={OutputSelectedToken}
             setSelectedToken={(token) => setOutputSelectedToken(token)}
           />
+          <TokenInput
+            tokens={tokens}
+            selectedToken={InputSelectedToken}
+            setSelectedToken={(token) => setInputSelectedToken(token)}
+          />
         </Card>
-        <SwapFetchingCard children={<div>fetching best price...</div>} />
-
-        <SwapButton />
-
+        <Card>
+          <div className="text-lightBlack text-left">You receive</div>
+          <TokenInput
+            tokens={tokens}
+            selectedToken={OutputSelectedToken}
+            setSelectedToken={(token) => setOutputSelectedToken(token)}
+          />
+        </Card>
+        <SwapFetchingCard>
+          <div>fetching best price...</div>
+        </SwapFetchingCard>
+        <SwapBtn tokenName={"Select Token"} />
         {/* 조건 1.지갑연동 안됐을때 wallet 연결 유도 2. 토큰 두개다 골랐는데 Input or Output 입력안됐을때 "Enter an amount" 3. Input or Output 이 입력됐을때 계산실행해주기 "fetching best price 표시 -> 입력됐을때 얼마로 바꿔줄수있는지 표시 "1UNI = 3.234 WETH" 4. 내가 보유한 첫번째 TokenInput 의 balance 가 InputValue 보다 높을때는 "Insufficient WETH balance" 띄어주고 swap 막기 5.위의 조건을 다 피해가면 그때 "Swap"가능 */}
       </div>
     </SwapContainer>
