882c99a94e782b95b007e441916663001fb61e02
youdonghee <0103donghee@naver.com>
2023-11-24 14:15:45 +0900

    governance -> CreateProposal UI 완료

--- src/pages/Governance/CreateProposal/index.tsx
+++ src/pages/Governance/CreateProposal/index.tsx
@@ -3,6 +3,9 @@ import { useNavigate } from "react-router-dom";
 import Container from "src/components/container";
 import { Divstyle } from "src/components/Pairname/Pairname.style";
 import AddLiquidity from "src/contents/poolpair/Liquidity/AddLiquidity";
+import ProposalForm from "src/contents/Proposal/ProposalForm";
+import SubmitButton from "src/contents/Proposal/SubmitButton";
+import TipSection from "src/contents/Proposal/TipSection";
 import { Divstyles } from "src/pages/StakeDetail/StakeDetail.style";
 
 const CreateProposal = () => {
@@ -25,20 +28,9 @@ const CreateProposal = () => {
           <div className=" min-w-[340px] pc:w-[80%] mobile:w-[85%] p-5 moblie:mt-3 bg-cardWhite dark:bg-D_cardWhite rounded-xl: rounded-bodyBackRadius m-5 mt-16 shadow-md">
             <div className="font-semibold text-[20px]">CreateProposal</div>
             <div className="w-full flex flex-col flex-wrap p-[20px]">
-              <div className="w-full bg-[#9CE084] rounded-[20px] text-xl flex items-center justify-center shadow-md p-2">
-                <div className="text-deepGreen">
-                  <strong>Tip:</strong>
-                  Select an action and describe your proposal for the community.
-                  The proposal cannot be modified after submission, so please
-                  verify all information before submitting. The voting period
-                  will begin immediately and last for 7 days. To propose a
-                  custom action, read the docs.
-                </div>
-              </div>
-
-              <div className="bg-cardWhite mt-[10px] pt-3 pb-3 pl-4 pr-4 rounded-[20px] border-[2px] border-[#22222212]">
-                dd
-              </div>
+              <TipSection />
+              <ProposalForm />
+              <SubmitButton />
             </div>
           </div>
         </div>
