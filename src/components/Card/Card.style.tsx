b689898b5782da15d549c12228bd26e48ce4dcce
wijiwon <dnlwldnjs5@gmail.com>
2023-11-24 09:54:17 +0900

    UI: card style 변경 / Router: tokenDetail 경로 변경

--- src/components/Card/Card.style.tsx
+++ src/components/Card/Card.style.tsx
@@ -1,3 +1,3 @@
 export const Divstyle = {
-  cardBox: `min-w-[340px] max-w-[500px] pc:w-[85%] mobile:w-[85%] p-5 moblie:mt-3 bg-cardWhite dark:bg-D_cardWhite rounded-xl: rounded-bodyBackRadius items-center m-2 shadow-md`,
+  cardBox: `min-w-[340px] pc:w-[85%] mobile:w-[85%] p-5 moblie:mt-3 bg-cardWhite dark:bg-D_cardWhite rounded-xl: rounded-bodyBackRadius items-center m-5 shadow-md`,
 };
