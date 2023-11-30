67f0c02cb658dafa19d226687cb92af0ca403c1a
youdonghee <0103donghee@naver.com>
2023-11-30 21:31:27 +0900

    UI: serarch 수정중

--- src/layout/HeaderBox/SearchBar/index.tsx
+++ src/layout/HeaderBox/SearchBar/index.tsx
@@ -18,8 +18,7 @@ const SearchBox = () => {
 
   const nav = useNavigate();
 
-  const { web3, dataContract, pairContract } =
-    useWeb3(window.ethereum);
+  const { web3, dataContract, pairContract } = useWeb3(window.ethereum);
 
   const getPoolData = async () => {
     if (!pairContract || !dataContract || !web3) {
@@ -125,61 +124,69 @@ const SearchBox = () => {
   }
 
   return (
-    <div className="flex-col relative  justify-center w-[25%] mobile:w-[30%] header:hidden mobile:block">
+    <div className="flex-col relative justify-center w-[25%] mobile:w-[30%] header:hidden mobile:block">
       <div className="flex">
-        <div className="pc:w-[331px] w-full relative h-[46px] rounded-[63px] overflow-hidden border-[3px] border-baseWhite shadow-[0px_4px_5px_#00000040]">
+        <div className="pc:w-[331px] w-full relative h-[46px] rounded-[63px] overflow-hidden border-[3px] border-baseWhite shadow-[0px_4px_5px_#00000040] pc:bg-[#c9f399] mobile:bg-[#a8e99d]">
           <img
             className="absolute w-[22px] h-[21px] top-[10px] left-[15px]"
             alt="Search icon"
             src="/images/search.svg"
           />
           <input
-            className="absolute w-full h-full left-0 top-0 pl-[48px] pr-3 py-0 opacity-80 [font-family:'Inter-Bold',Helvetica] font-bold text-baseWhite text-[19px] tracking-[0] leading-[normal] placeholder-baseWhite bg-transparent border-none outline-none"
+            className="w-full h-full left-0 top-0 pl-[48px] pr-3 py-3 px-4 opacity-80 font-bold text-baseWhite text-[19px] tracking-[0] placeholder-baseWhite bg-transparent border-none focus:outline-none focus:ring-0 "
             placeholder="Search"
             type="text"
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
           />
         </div>
-      </div>
-      {/* <div className="absolute top-full left-0 bg-lime-500 z-10 w-full max-h-[300px] overflow-auto"> */}
-      <div className="pc:w-[331px] w-full absolute top-full left-0 bg-lime-500 z-10 max-h-[300px] overflow-auto  border-baseWhite">
-        {searchResults.length > 0 && (
-          <ul>
-            {searchResults.map((token, index) => (
-              <li
-                key={index}
-                className="cursor-pointer hover:text-white"
-                onClick={() => searchClickHandler(token.tokenSymbol)}
-              >
-                {token.tokenUri && token.tokenUri.includes("+") ? (
-                  token.tokenUri
-                    .split("+")
-                    .map((uri, uriIndex) => (
+        {/* <div className="rounded-b-3xl border-x-3 border-baseWhite text-baseWhite pc:w-[331px] w-full absolute top-[20px] left-0 z-[-100] max-h-[300px] overflow-auto pc:bg-[#c9f399] mobile:bg-[#a8e99d] border-[3px] pt-[30px]">
+         */}
+        <div
+          className={`rounded-b-3xl text-baseWhite pc:w-[331px] w-full absolute top-[20px] left-0 z-[-100] max-h-[200px] overflow-auto pc:bg-[#c9f399] mobile:bg-[#a8e99d] pt-[30px] ${
+            searchResults.length > 0
+              ? "border-[3px] border-x-3 border-baseWhite"
+              : ""
+          }`}
+        >
+          {searchResults.length > 0 && (
+            <ul className="overflow-y-auto h-full">
+              {searchResults.map((token, index) => (
+                <li
+                  key={index}
+                  className="flex items-center px-4 h-[35px] font-bold cursor-pointer hover:text-deepGreen"
+                  onClick={() => searchClickHandler(token.tokenSymbol)}
+                >
+                  {token.tokenUri && token.tokenUri.includes("+") ? (
+                    token.tokenUri.split("+").map((uri, uriIndex) => (
+                      //! tokenUrI 한개씩 뽑아올수있는지 물어보기
                       <img
                         key={uriIndex}
                         src={uri}
                         alt={`${token.tokenSymbol}-${uriIndex}`}
-                        className="inline-block w-[25px] h-[25px] rounded-full border-[1px] border-gray-300"
+                        className="inline-block w-[28px] h-[28px] rounded-full border-[1px] border-baseWhite mr-2"
                       />
                     ))
-                ) : (
-                  <img
-                    src={token.tokenUri}
-                    alt={token.tokenSymbol}
-                    className="inline-block w-[25px] h-[25px] rounded-full border-[1px] border-gray-300"
-                  />
-                )}
+                  ) : (
+                    <img
+                      src={token.tokenUri}
+                      alt={token.tokenSymbol}
+                      className="inline-block w-[28px] h-[28px] rounded-full border-[1px] border-baseWhite mr-2"
+                    />
+                  )}
 
-                {token.tokenName ? (
-                  <span>{`${token.tokenName}/${token.tokenSymbol}`}</span>
-                ) : (
-                  <span>{token.tokenSymbol}</span>
-                )}
-              </li>
-            ))}
-          </ul>
-        )}
+                  <span className="text-lg [text-shadow:0px_2px_3px_#00000040]">
+                    {token.tokenName ? (
+                      <span>{`${token.tokenName}/${token.tokenSymbol}`}</span>
+                    ) : (
+                      <span>{token.tokenSymbol}</span>
+                    )}
+                  </span>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
       </div>
     </div>
   );
