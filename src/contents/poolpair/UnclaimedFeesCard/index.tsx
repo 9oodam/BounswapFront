21aac708cc3b6bf71a0a030fcd33b12b2a2a4925
9oodam <godamcity98@gmail.com>
2023-12-01 11:51:09 +0900

    topPool -> myPool btn 생성

--- src/contents/poolpair/UnclaimedFeesCard/index.tsx
+++ src/contents/poolpair/UnclaimedFeesCard/index.tsx
@@ -36,24 +36,24 @@ const UnclaimedFeesCard: React.FC<{
           <div className="pc:w-[85%] mobile:w-[85%] p-5 bg-cardWhite dark:bg-D_cardWhite rounded-xl: rounded-bodyBackRadius items-center pc:m-5 mobile:mt-3 shadow-md ">
             <div className="flex flex-col">
               <div className="flex justify-between mb-4">
-                <div className="flex items-center text-deepBlack">
+                <div className="flex items-center text-deepBlack dark:text-baseWhite">
                   <img
                     src={pool.token0Uri}
                     className="w-[30px] rounded-full mr-3"
                   />
                   <div>{pool.token0Symbol}</div>
                 </div>
-                <div className="mr-3 text-deepBlack">{fee.token0FeeAmount}</div>
+                <div className="mr-3 text-deepBlack dark:text-baseWhite">{fee.token0FeeAmount}</div>
               </div>
               <div className="flex justify-between">
-                <div className="flex items-center text-deepBlack">
+                <div className="flex items-center text-deepBlack dark:text-baseWhite">
                   <img
                     src={pool.token1Uri}
                     className="w-[30px] rounded-full mr-3"
                   />
                   <div>{pool.token1Symbol}</div>
                 </div>
-                <div className="mr-3 text-deepBlack">{fee.token1FeeAmount}</div>
+                <div className="mr-3 text-deepBlack dark:text-baseWhite">{fee.token1FeeAmount}</div>
               </div>
             </div>
           </div>
